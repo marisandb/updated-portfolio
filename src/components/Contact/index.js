10e55c11b82cd19d53f2a9cb819ddfad0334b7b0
maris <marisandb@yahoo.com>
2022-02-19 23:28:43 -0600

    finished CSS styling for contact and added icons

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -2,26 +2,40 @@ import React from 'react';
 import "./style.css"
 import { HiOutlineMail } from 'react-icons/hi';
 import { FaPhoneAlt } from 'react-icons/fa';
+import { AiFillLinkedin } from 'react-icons/ai';
+import { AiFillGithub } from 'react-icons/ai';
+import { IconContext } from "react-icons";
+
 
 function Contact() {
 
   return (
     <section>
-     <table>
-      <tr>
-        <td>
-        <FaPhoneAlt />
-          Phone:
-        </td>
-        <td>608-323-0979</td>
-      </tr>
-      <tr>
-        <td>
-        <HiOutlineMail />
-          Email:</td>
-        <td><a href="mailto:marisandb@yahoo.com">marisandb@yahoo.com</a></td>
-      </tr>
-    </table>
+      <h1 className='contact-header'>If you have questions, or would just like to get to know me better, please reach out through your preffered method listed below. <br /> I look forward to hearing from you!</h1>
+      <div className='contact-info'>
+        <div>
+        <p><FaPhoneAlt /> Phone: 608-323-0979</p> 
+        </div>
+        <div>
+        <p>
+        <HiOutlineMail /> Email: 
+        <a href="mailto:marisandb@yahoo.com">marisandb@yahoo.com</a>
+        </p>
+        </div>
+      </div>
+
+    <div className='icons'>
+    <IconContext.Provider value={{ className: 'react-icons', color: "black", size: "45px"}}>
+      <div>
+    <a href="https://www.linkedin.com/in/marisa-bartelt-28119b20a/">
+        <AiFillLinkedin/>
+    </a>
+    <a href="https://github.com/marisandb">
+        <AiFillGithub/>
+    </a>
+      </div>
+    </IconContext.Provider>
+    </div>
     </section>
   );
 }
